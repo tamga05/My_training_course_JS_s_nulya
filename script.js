698b58c51f2e3af7651f4197339b863f7b6ff82f
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-06 14:08:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log("Привет, Андрей!!!");
+
+// window.alert("Привет, Андрей!!!");
+
+// document.getElementById
+
+let myVar1;
+myVar1 = 'myDiv';
+
+let viewVar1;
+viewVar1 = document.getElementById(myVar1);
+
+window.alert(viewVar1.id);
+window.alert(viewVar1.className);
+window.alert(viewVar1.title);
+window.alert(viewVar1.style);
